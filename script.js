52487c09403a92dbfe167a2d28ba79fea9239e6c
Keenan <82282404+dazeitem@users.noreply.github.com>
2024-08-02 11:54:33 +0200

    Script.js updates
    - choices added
    - scores added
    - computer choice generator added

---
+++ script.js
@@ -0,0 +1,16 @@
+const choices = ["rock", "paper", "scissors"]
+
+let humanScore = 0
+let computerScore = 0
+
+function getComputerChoice() {
+    return choices[Math.floor(Math.random() * choices.length)]
+}
+
+function getHumanChoice() {
+    return 
+}
+
+function playRound(computerChoice, humanChoice) {
+
+}
